325ce826393985b7d4790937ca2c116f8346a3a5
Bogdan Belogurov <bogdan.belogurov@emlid.com>
2023-11-30 20:52:43 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# XrayWeave
+
+A description of this package.
---
+++ Tests/XrayWeaveTests/XrayWeaveTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import XrayWeave
+
+final class XrayWeaveTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(XrayWeave().text, "Hello, World!")
+    }
+}
